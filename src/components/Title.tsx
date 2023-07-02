731461f6956801373c614dcd5fc0a2e023c25cbc
Isaev.N <nikitavegas95@gmail.com>
2023-07-02 17:14:48 +0300

    Скобки убрал, регулярное вынес.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import '../styles/main.scss'
 
 const Title:FC = () => (
-  <h2 className={'content__title'}>Авторизация</h2>
+  <h2 className='content__title'>Авторизация</h2>
 );
 
 export default Title
